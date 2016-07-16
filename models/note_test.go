ade4dda8de1176ab0993ef95253b747daa187451
Janne Vikstedt <janne.vikstedt@eficode.com>
2016-07-16 18:45:59 +0300

    Use t.Error instead of panic on tests

--- models/note_test.go
+++ models/note_test.go
@@ -9,6 +9,6 @@ func TestGetAllNotes(t *testing.T) {
 	db.Setup("../db/dbconf.yml")
 	notes := GetAllNotes()
 	if len(notes) > 0 {
-		panic("Should return empty array, no data on database")
+		t.Error("Expected length 0, got: ", len(notes))
 	}
 }
