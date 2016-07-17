d66840df350e2a85d85b278b42dcf28c5d8d81c3
Janne Vikstedt <janne.vikstedt@eficode.com>
2016-07-17 19:40:31 +0300

    Router setup to it's own function

--- main.go
+++ main.go
@@ -15,14 +15,18 @@ func main() {
 	if port == "" {
 		port = "8000"
 	}
+
+	n := negroni.Classic()
+	n.UseHandler(setupRouter())
+	n.Run(":" + port)
+}
+
+func setupRouter() *httprouter.Router {
 	router := httprouter.New()
 
 	notesController := controllers.NotesController{}
-
 	router.GET("/notes", notesController.Index)
 	router.POST("/notes", notesController.Create)
 
-	n := negroni.Classic()
-	n.UseHandler(router)
-	n.Run(":" + port)
+	return router
 }
